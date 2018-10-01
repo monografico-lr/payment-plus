b09bfe204d4ff14e30782e7e443134395ed0ecf7
Jesus Guerrero <jesusant.guerrero@gmail.com>
2018-10-01 14:10:17 -0400

    crea migracion de la tabla de pagos

--- database/migrations/2018_09_22_190925_create_averiass_table.php
+++ database/migrations/2018_09_22_190925_create_averiass_table.php
@@ -6,33 +6,33 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateAveriassTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('averias', function (Blueprint $table) {
-			$table->increments('id');
-			$table->integer('id_cliente');
-			$table->integer('id_contrato');
-			$table->text('descripcion');
-			$table->date('fecha_reparacion');
-			$table->integer('estado')->comment('valores: por reparar, reparado'); // por reparar, reparado
-			$table->char('tecnico', 100);
-			$table->softDeletes();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('averias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_cliente');
+            $table->integer('id_contrato');
+            $table->text('descripcion');
+            $table->date('fecha_reparacion');
+            $table->integer('estado')->comment('valores: por reparar, reparado')->default(1); // por reparar, reparado
+            $table->char('tecnico', 100);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('averias');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('averias');
+    }
 }
--- database/migrations/2018_09_22_195420_create_contratos_table.php
+++ database/migrations/2018_09_22_195420_create_contratos_table.php
@@ -6,43 +6,43 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateContratosTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('contratos', function (Blueprint $table) {
-			$table->increments('id');
-			$table->integer('id_user');
-			$table->integer('id_cliente');
-			$table->integer('id_servicio');
-			$table->string('codigo', 6);
-			$table->date('fecha');
-			$table->integer('duracion');
-			$table->integer('estado')->comment('activo, saldado, cancelado, mora, suspendido');
-			$table->string('nombre_equipo', 50);
-			$table->macAddress('mac_equipo');
-			$table->string('modelo', 50);
-			$table->string('router', 50);
-			$table->macAddress('mac_router');
-			$table->ipAddress('ip');
-			$table->boolean('instalado');
-			$table->text('observaciones');
-			$table->date('fecha_suspencion');
-			$table->softDeletes();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_user');
+            $table->integer('id_cliente');
+            $table->integer('id_servicio');
+            $table->string('codigo', 6);
+            $table->date('fecha');
+            $table->integer('duracion');
+            $table->integer('estado')->comment('activo, saldado, cancelado, mora, suspendido')->default(1);
+            $table->string('nombre_equipo', 50);
+            $table->macAddress('mac_equipo');
+            $table->string('modelo', 50);
+            $table->string('router', 50);
+            $table->macAddress('mac_router');
+            $table->ipAddress('ip');
+            $table->boolean('instalado');
+            $table->text('observaciones');
+            $table->date('fecha_suspencion');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('contratos');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contratos');
+    }
 }
